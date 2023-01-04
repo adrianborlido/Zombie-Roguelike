6a3e1fe2d16a7c8f9bc8869341473c117aaa2b33
Adrian Borlido <borlidoadrian@gmail.com>
2023-01-03 23:13:42 -0300

    Player movement refactor, added new assets, collision and cameraMotor script

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,65 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player: MonoBehaviour {
 
-    [SerializeField] int health = 4;
-    [SerializeField] int damage = 1;
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+    private float moveSpeed = 4;
+    private RaycastHit2D hit;
 
-    // Movement
-    [SerializeField] float _Speed = 4;
-    [SerializeField] Camera _Camera;
-    PlayerInput _Input;
-    Vector2 _Movement;
-    Vector2 _MousePosition;
-    Rigidbody2D _Rigidbody;
-
-    private void Awake() {
-        _Input = new PlayerInput();
-        _Rigidbody = GetComponent<Rigidbody2D>();
-    }
-
-    private void OnEnable() {
-        _Input.Enable();
-        _Input.Gameplay.Movement.performed += OnMovement;
-        _Input.Gameplay.Movement.canceled += OnMovement;
-        _Input.Gameplay.MousePosition.performed += OnMousePosition;
-    }
-
-    private void OnDisable() {
-        _Input.Disable();
-    }
-
-    private void OnMovement(InputAction.CallbackContext context) {
-        _Movement = context.ReadValue<Vector2>();
-    }
-    private void OnMousePosition(InputAction.CallbackContext context) {
-        _MousePosition = _Camera.ScreenToWorldPoint(context.ReadValue<Vector2>());
+    // Start is called before the first frame update
+    void Start() {
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void FixedUpdate() {
-        _Rigidbody.velocity = _Movement * _Speed;
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        moveDelta = new Vector2(x, y);
 
-        // If you want to keep accelarating at the end of the movement, use this instead
-        // _Rigidbody.AddForce(_Movement * _Speed);
-
-        Vector2 facingDirection = _MousePosition - _Rigidbody.position;
-        float angle = Mathf.Atan2(facingDirection.y, facingDirection.x) * Mathf.Rad2Deg;
-        _Rigidbody.MoveRotation(angle);
-    }
-
-    private void CheckIfGameOver() {
-        if(health <= 0) {
-            // TODO: - Trigger player death animation
-            // Finish game
+        if(moveDelta.x > 0) {
+            transform.localScale = Vector3.one;
+        } else if(moveDelta.x < 0) {
+            transform.localScale = new Vector3(-1, 1, 1);
         }
-    }
 
-    public void LoseHealth(int loss) {
-        //TODO: - Trigger player hit animation
-        health -= loss;
-        CheckIfGameOver();
+        hit = Physics2D.BoxCast(
+            transform.position,
+            boxCollider.size,
+            0,
+            new Vector2(0, moveDelta.y),
+            Mathf.Abs(moveDelta.y * moveSpeed * Time.deltaTime),
+            LayerMask.GetMask("Unit", "BlockingLayer"));
+        if(hit.collider == null) {
+            transform.Translate(0, moveDelta.y * moveSpeed * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(
+            transform.position,
+            boxCollider.size,
+            0,
+            new Vector2(moveDelta.x, 0),
+            Mathf.Abs(moveDelta.x * moveSpeed * Time.deltaTime),
+            LayerMask.GetMask("Unit", "BlockingLayer"));
+        if(hit.collider == null) {
+            transform.Translate(moveDelta.x * moveSpeed * Time.deltaTime, 0, 0);
+        }
     }
 }
