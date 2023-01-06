21316166273387d30f2ad6a994059da25043d3e8
Adrian Borlido <borlidoadrian@gmail.com>
2023-01-05 23:21:46 -0300

    Modified MainScene

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -3,5 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy: Mover {
+    // Hitbox
+    private BoxCollider2D hitbox;
+    private Collider2D[] hits = new Collider2D[10];
 
+    
 }
